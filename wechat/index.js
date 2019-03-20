07d2b6d5392225b17720c55f6f8d508d60106914
WangLi <wangli@qq.com>
2019-03-20 21:18:35 +0800

    '得到最终的access_token'

--- wechat/index.js
+++ wechat/index.js
@@ -1,8 +1,8 @@
 /*定义获取access_token的模块*/
 const rp = require('request-promise-native');
-const {writeFile} = require('fs');
+const {writeFile, readFile} = require('fs');
 
-async function getaccessToken() {
+async function getAccessToken() {
 
     const appId = 'wxe4c8a7b61d8bec3a';
     const appSecret = '787e8a6947d46bd0542fa73892dc04b4';
@@ -15,16 +15,41 @@ async function getaccessToken() {
     result.expires_in = Date.now() + 7200000 - 300000;
 
     //保存下来
-    writeFile('./accessToken.txt', JSON.stringify( result), err => {
+    writeFile('./accessToken.txt', JSON.stringify(result), err => {
         if (!err) console.log('文件保存成功了~~');
         else console.log(err);
     })
 
     //返回获取好的access_token
     return result;
+}
 
-    console.log(result);
+//定义最终得到的access_token的函数
+function fetchAccessToken() {
+    return new Promise((resolve, reject) => {
+        readFile('accessToken.txt', (err, data) => {
+            if (!err) {
+                resolve(JSON.parse(data.toString()));
+            } else {
+                reject(err);
+            }
+        })
+    })
+        .then((res) => {
+            if (res.expires_in < Date.now()) {
+                return getAccessToken();
+            } else {
+                return res;
+            }
+        })
+        .catch(err => {
+            return getAccessToken();
+        })
 
 }
 
-getaccessToken();
+/*(async () => {
+    const result = await fetchAccessToken();
+    console.log(result);
+})()*/
+
